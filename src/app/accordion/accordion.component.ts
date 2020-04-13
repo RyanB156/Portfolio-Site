3609aa866efea3c12003016013bbda30bdcbd595
RyanB156 <bressetteryan@gmail.com>
2020-04-13 08:25:44 -0400

    Added new accordion and panel components to make accordions easier to make.

---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,28 @@
+import { Component, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { PanelComponent } from '../panel/panel.component';
+ 
+@Component({
+  selector: 'accordion',
+  template: '<ng-content></ng-content>'
+})
+export class AccordionComponent  implements AfterContentInit {
+  @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
+ 
+  ngAfterContentInit() {
+    // Loop through all panels
+    this.panels.toArray().forEach((panel: PanelComponent) => {
+      // subscribe panel toggle event
+      panel.toggle.subscribe(() => {
+        // Open the panel
+        this.clickPanel(panel);
+      });
+    });
+  }
+ 
+  clickPanel(panel: PanelComponent) {
+    // close all panels
+    //this.panels.toArray().forEach(p => p.opened = false);
+    // open the selected panel
+    panel.opened = !panel.opened;
+  }
+}
---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+ 
+@Component({
+  selector: 'panel',
+  styleUrls: [
+    './panel.component.scss'
+  ],
+  template: `
+  <div class="panel panel-info">
+    <div class="panel-heading btn-link" (click)="toggle.emit()">
+      {{title}}
+    </div>
+    <div class="panel-body" *ngIf="opened">
+      <ng-content></ng-content>
+    </div>
+  <div>
+  `
+})
+export class PanelComponent {
+  @Input() opened = false;
+  @Input() title: string;
+  @Output() toggle: EventEmitter<any> = new EventEmitter<any>();
+}
