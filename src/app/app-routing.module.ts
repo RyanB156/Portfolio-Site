17b08c10db5057f126ed2152892b543afc6437d6
RyanB156 <bressetteryan@gmail.com>
2020-04-13 08:28:35 -0400

    Remove routing and imports for the removed About component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SnakeComponent } from './snake/snake.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { TribeComponent } from './tribe/tribe.component';
 import { LogicsimComponent } from './logicsim/logicsim.component';
 import { HitmanComponent } from './hitman/hitman.component';
@@ -12,7 +11,6 @@ import { HazelComponent } from './hazel/hazel.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'games/snake', component: SnakeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'projects/tribe', component: TribeComponent },
   { path: 'projects/logicsim', component: LogicsimComponent },
   { path: 'projects/hitman', component: HitmanComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SnakeComponent } from './snake/snake.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TribeComponent } from './tribe/tribe.component';
 import { LogicsimComponent } from './logicsim/logicsim.component';
 import { HazelComponent } from './hazel/hazel.component';
 import { HitmanComponent } from './hitman/hitman.component';
+import { PanelComponent } from './panel/panel.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImageboxComponent } from './imagebox/imagebox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SnakeComponent,
     HomeComponent,
-    AboutComponent,
     SidebarComponent,
     TribeComponent,
     LogicsimComponent,
     HazelComponent,
-    HitmanComponent
+    HitmanComponent,
+    PanelComponent,
+    AccordionComponent,
+    FooterComponent,
+    ImageboxComponent,
   ],
   imports: [
     BrowserModule,
